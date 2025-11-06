5e358421326fee1c0877b8533a1ae524ac7d841a
Lily McCabe <lily@lilymccabe.ca>
2025-11-06 12:44:45 -0500

    Revert "Fix typo in fallback implementation"
    This reverts commit 0dfd5a87ab6628bb428302489bf896357b909d29.

--- src/load-file/load.c
+++ src/load-file/load.c
@@ -112,8 +112,8 @@ void unload_file(struct LoadedFile *file) {
 
 
 struct LoadedFile {
-  const char* data;
-  size_t length;
+  const char* const data;
+  const size_t length;
 };
 struct LoadedFile* load_from_filename(const char* const filename) {
   FILE* file = fopen(filename, "r");
@@ -165,7 +165,7 @@ struct LoadedFile* load_from_filename(const char* const filename) {
 
   fclose(file);
 
-  struct LoadedFile output_data = {data, filesize};
+  struct LoadedFile output_data = {mapping, len};
   struct LoadedFile *output = malloc(sizeof(struct LoadedFile));
   *output = output_data;
   return output;
