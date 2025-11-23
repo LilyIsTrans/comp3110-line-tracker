b5b08f963e1664b22271e5c43740f87272d20cf6
Lily McCabe <lily@lilymccabe.ca>
2025-11-23 14:16:02 -0500

    Define the core substring interfaces

--- src/string-slice/string-slice.h
+++ src/string-slice/string-slice.h
@@ -8,6 +8,10 @@
   * This structure contains a reference to some text somewhere in readable memory.
   * The substring pointed to might not be null terminated, so standard string
   * functions are very dangerous and should not be applied.
+  *
+  * The `end` pointer points to the first address NOT part of this substring (that is,
+  * `*(end)` is potentially undefined behaviour, `*(end - 1)` is the last character of
+  * the substring).
   */
 typedef struct {
   const char* const start;
@@ -29,4 +33,33 @@ inline size_t length(Substring);
   * \param[in] 2 The other substring to be compared
   */
 bool equal(Substring, Substring);
+
+
+/**
+  * \brief Just an array of substrings with tracking for the size.
+  */
+struct SubstringArray {
+  size_t len;
+  Substring array[];
+};
+
+
+/**
+  * \brief Allocates a new `SubstringArray` with a certain array size.
+  * To deallocate the array, just `free` it. This function is a thin wrapper on
+  * `malloc` to keep you from forgetting to add the size of the struct.
+  */
+inline struct SubstringArray* new_substring_array(const size_t size) {
+  return malloc(sizeof(struct SubstringArray) + size * sizeof(Substring));
+}
+
+
+/**
+  * \brief Returns an array of substrings from the haystack holding each line in the haystack.
+  * Does not include trailing `'\n'` or `'\r'` characters, but other whitespace is treated as
+  * though it were regular text. Empty lines will correspond to empty substrings, they will
+  * not be omitted from the array.
+  */
+struct SubstringArray* lines(const Substring haystack);
+
 
