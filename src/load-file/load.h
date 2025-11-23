fe19ca7462ba938a42fef222c55f057881efbac9
Lily McCabe <lily@lilymccabe.ca>
2025-11-23 13:52:43 -0500

    Move initial substring handling code to new slice library

--- src/load-file/load.h
+++ src/load-file/load.h
@@ -2,33 +2,6 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdbool.h>
-/**
-  * Holds a reference to a (POTENTIALLS NON NULL TERMINATED) substring.
-  * 
-  * This structure contains a reference to some text somewhere in readable memory.
-  * The substring pointed to might not be null terminated, so standard string
-  * functions are very dangerous and should not be applied.
-  */
-typedef struct {
-  const char* const start;
-  const char* const end;
-} Substring;
-
-/**
-  * \brief Determines the length (number of `char`s) in a Substring.
-  *
-  * \param[in] 1 The substring whose length is to be determined.
-  */
-inline size_t length(Substring);
-
-/**
-  * \brief Compares two substrings to determine if they are exactly equal
-  * (that is, contain precisely the same bytes in the same order).
-  *
-  * \param[in] 1 One substring to be compared
-  * \param[in] 2 The other substring to be compared
-  */
-bool equal(Substring, Substring);
 
 /**
   * Owns the entire contents of a loaded file.
--- src/string-slice/string-slice.c
+++ src/string-slice/string-slice.c
@@ -1,1 +1,22 @@
+#include "string-slice.h"
 
+size_t length(Substring str)  {
+  return str.end - str.start;
+}
+
+
+bool equal(Substring a, Substring b) {
+  size_t len;
+  if (a.start == b.start && a.end == b.end) {
+    return true;
+  }
+  // Use of the result of assignment as an operand to comparison intentional,
+  // this is a rare case where that's actually the most elegant way to write
+  // this.
+  else if ((len = length(a) != length(b))) {
+    return false;
+  }
+  else {
+    return !memcmp(a.start, b.start, len);
+  }
+}
