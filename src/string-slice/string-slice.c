24de1b292e8221802ce49c07b5ef07c1d91a9f37
Quinn <qafalconer@gmail.com>
2025-11-26 10:21:39 -0500

    Added comments

--- src/string-slice/string-slice.c
+++ src/string-slice/string-slice.c
@@ -5,27 +5,32 @@
 #include <stdlib.h>
 #include <string.h>
 
+//length of substring
 size_t length(Substring str)  {
   return str.end - str.start;
 }
 
-
+//check if substrings are equal
 bool equal(Substring a, Substring b) {
   size_t len;
 
   if (a.start == b.start && a.end == b.end) {
     return true;
   }
+
   // Use of the result of assignment as an operand to comparison intentional,
   // this is a rare case where that's actually the most elegant way to write
   // this.
   else if ((len = length(a) != length(b))) {
     return false;
   }
+  //if equal length, compare string contents
   else {
     return !memcmp(a.start, b.start, len);
   }
 }
+
+//create dynamic array to hold substrings
 struct SubstringArray *new_substring_array(const size_t capacity) {
   struct SubstringArray *output =
       malloc(sizeof(struct SubstringArray) + capacity * sizeof(Substring));
@@ -33,41 +38,48 @@ struct SubstringArray *new_substring_array(const size_t capacity) {
   output->len = 0;
   output->capacity = capacity;
 
+  //initialize substring array to 0 to prevent garbage values
   memset(output->array, 0, capacity);
   return output;
 }
 
+//appends substring to existing array
 struct SubstringArray* append_to_substring_array(struct SubstringArray* array, const Substring str) {
+  //if array is full, reallocate memory for larger array (double capacity)
   if (array->len == array->capacity) {
     struct SubstringArray* new_array = realloc(array, sizeof(struct SubstringArray) + array->capacity * 2 * sizeof(Substring));
     
+	//ERROR HANDLING
     if (!new_array) {
       fprintf(stderr, "Failed to realloc substring array of length %zu!\n", array->capacity);
       exit(EXIT_FAILURE);
     }
     array = new_array;
   }
-  array->array[array->len] = str;
-  array->len += 1;
+
+  array->array[array->len] = str;   //append new substring
+  array->len += 1;				    //increment length
   return array;
 }
 
+//splits substring (haystack) into lines based on '\n' characters
 struct SubstringArray* lines(Substring haystack) {
-  struct SubstringArray* output = new_substring_array(0);
+	struct SubstringArray* output = new_substring_array(0);  //new substring array to hold lines
   Substring last_line_so_far = haystack;
   last_line_so_far.end = last_line_so_far.start;
 
+  //iterate through haystack to find lines
   while (last_line_so_far.end < haystack.end) {
     if (*last_line_so_far.end == '\n') {
-      output = append_to_substring_array(output, last_line_so_far);
-      last_line_so_far.start = last_line_so_far.end + 1;
+      output = append_to_substring_array(output, last_line_so_far);   //append substring up to '\n' to output array
+	  last_line_so_far.start = last_line_so_far.end + 1;			  //update start pointer to character after '\n'
     }
     last_line_so_far.end++;
   }
   return output;
 }
 
-
+//calculates FNV-1a hash for substrings
 uint32_t fnv1a_hash(Substring str) {
   const uint32_t FNV_OFFSET_BASIS = 0x811c9dc5; // Pulled from https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function#FNV_hash_parameters
   const uint32_t FNV_PRIME = 0x01000193; // Pulled from https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function#FNV_hash_parameters
@@ -83,7 +95,7 @@ uint32_t fnv1a_hash(Substring str) {
   
 }
 
-
+//allocate memory for HashArray struct
 struct HashArray *new_hash_array(size_t capacity) {
   struct HashArray *output =
       malloc(sizeof(struct HashArray) + capacity * sizeof(uint32_t));
@@ -95,6 +107,8 @@ struct HashArray *new_hash_array(size_t capacity) {
   return output;
   
 }
+
+//allocate memeory for HashArray from SubstringArray and fill with hashes
 struct HashArray *new_hash_array_from_substring_array(const struct SubstringArray *str_array) {
   struct HashArray *output =
       malloc(sizeof(struct HashArray) + str_array->len * sizeof(uint32_t));
@@ -110,6 +124,7 @@ struct HashArray *new_hash_array_from_substring_array(const struct SubstringArra
   
 }
 
+//define an entry in hash table
 struct SubstringHashTableEntry {
   Substring str;
   size_t *line_numbers;
@@ -126,6 +141,7 @@ struct SubstringHashTableEntry {
   uint32_t hash;
 };
 
+//define the layout of hash table
 struct SubstringHashTable {
   /// Must always be a power of 2
   size_t capacity;
@@ -134,8 +150,7 @@ struct SubstringHashTable {
   struct SubstringHashTableEntry table[];
 };
 
-
-
+//allocate memory for SubstringHashTable struct
 struct SubstringHashTable *new_substring_hash_table(size_t capacity) {
   struct SubstringHashTable *output =
       malloc(sizeof(struct SubstringHashTable) + capacity * sizeof(struct SubstringHashTableEntry));
@@ -148,11 +163,11 @@ struct SubstringHashTable *new_substring_hash_table(size_t capacity) {
   
 }
 
-
+//define actions for linear probing (resolving collisions in hash table)
 enum LinearProbingAction {
-  INITIALIZE_THIS_ENTRY,
-  APPEND_TO_THIS_ENTRY,
-  PROBE_NEXT_ENTRY,
+  INITIALIZE_THIS_ENTRY,  //current entry is empty, initialize it
+  APPEND_TO_THIS_ENTRY,   //current entry matches, append line number
+  PROBE_NEXT_ENTRY,       //current entry occupied but does not match, probe next entry
 };
 
 
@@ -160,34 +175,37 @@ enum LinearProbingAction {
 /// Tells you what to do next in the linear probing process to insert
 /// the substring `str` into this entry.
 enum LinearProbingAction check_entry(struct SubstringHashTableEntry *entry, Substring str, uint32_t hash) {
-  if (entry->line_numbers == NULL) {
-    return INITIALIZE_THIS_ENTRY;
-  }
-  else if (hash == entry->hash && equal(str, entry->str)) {
-    return APPEND_TO_THIS_ENTRY;
-  }
-  else {
-    return PROBE_NEXT_ENTRY;
-  }
+    //initialize entry if empty
+    if (entry->line_numbers == NULL) {
+        return INITIALIZE_THIS_ENTRY;
+    }
+    //append new line number if hash and substring match
+    else if (hash == entry->hash && equal(str, entry->str)) {
+        return APPEND_TO_THIS_ENTRY;
+    }
+    //check next hash table entry
+    else {
+        return PROBE_NEXT_ENTRY;
+    }
 }
 
-
+//insert substring into hash table without reallocating (assume sufficient memory is available)
 void substring_hash_table_no_realloc_insert(struct SubstringHashTable* table, Substring str, size_t line_number_len, size_t line_numbers[line_number_len]) {
   const uint32_t hash = fnv1a_hash(str);
 
+  //if table is over half full, print warning
   if (table->load >= table->capacity / 2) {
     fprintf(stderr, "WARNING: Attempting non-realloc insert to overfull hash table. This is a bad idea, and might infinitely loop.\n");
   }
   
-
   size_t index = hash & (table->capacity - 1);
 
-
+  //linear probing to find appropriate entry
   while (true) {
     struct SubstringHashTableEntry* entry = &table->table[index];  
-
     
     switch (check_entry(entry, str, hash)) {
+
       case INITIALIZE_THIS_ENTRY:
         entry->hash = hash;
         entry->str = str;
@@ -196,11 +214,13 @@ void substring_hash_table_no_realloc_insert(struct SubstringHashTable* table, Su
         memcpy(entry->line_numbers, line_numbers, line_number_len * sizeof(size_t));
         table->load++;
         return;
+
       case APPEND_TO_THIS_ENTRY:
         entry->line_numbers = realloc(entry->line_numbers, sizeof(size_t) * (entry->line_numbers_len + line_number_len)); // Assume sufficient memory
         memcpy(entry->line_numbers + entry->line_numbers_len, line_numbers, sizeof(size_t) * line_number_len);
         entry->line_numbers_len += line_number_len;
         return;
+
       case PROBE_NEXT_ENTRY:
         index = (index + 1) & (table->capacity - 1);
         break; // From the switch, not the loop
@@ -213,15 +233,19 @@ void substring_hash_table_no_realloc_insert(struct SubstringHashTable* table, Su
 struct SubstringHashTable *reallocate_substring_hash_table(struct SubstringHashTable* old_table) {
   struct SubstringHashTable* new_table = new_substring_hash_table(old_table->capacity * 2);
   size_t entries_moved = 0;
+  
+  //copy entries from old table to new table
   for (size_t i = 0; i < old_table->capacity && entries_moved < old_table->load; ++i) {
     if (old_table->table[i].line_numbers != NULL) {
       substring_hash_table_no_realloc_insert(new_table, old_table->table[i].str, old_table->table[i].line_numbers_len, old_table->table[i].line_numbers);
     }
   }
+
   free(old_table);
   return new_table;
 }
 
+//reallocate hash table if necessary (more than half full)
 struct SubstringHashTable *insert_into_substring_hash_table(struct SubstringHashTable* table, Substring str, size_t line_number) {
   if (table->load >= table->capacity / 2) {
     table = reallocate_substring_hash_table(table);
@@ -233,7 +257,7 @@ struct SubstringHashTable *insert_into_substring_hash_table(struct SubstringHash
   
 }
 
-
+//get line numbers for given substring from hash table
 struct SimpleSizeTArray substring_hash_table_get_entry(struct SubstringHashTable* table, Substring str) {
   const uint32_t hash = fnv1a_hash(str);
 
@@ -265,27 +289,30 @@ struct SimpleSizeTArray substring_hash_table_get_entry(struct SubstringHashTable
   }
 }
 
-
-
-
+//get info about hash table performance
 struct HashTablePerformanceHeuristics* get_substring_hash_table_performance_heuristics(struct SubstringHashTable *table) {
   struct HashTablePerformanceHeuristics *output = malloc(sizeof(struct HashTablePerformanceHeuristics));
-  output->capacity = table->capacity;
-  output->load = table->load;
-  output->clusters = 0;
-  output->entries_not_at_home = 0;
-  output->max_cluster_size = 0;
+  
+  output->capacity = table->capacity;   //max num of entries table can hold
+  output->load = table->load;           //num of entries currently in table
+  output->clusters = 0;                 //num of clusters of entries
+  output->entries_not_at_home = 0;      //num of entries not at expected position
+  output->max_cluster_size = 0;         //size of largest cluster
 
   bool currently_in_cluster = false;
   size_t current_cluster_size = 0;
+
+  //calculate cluster info
   for (size_t i = 0; i < table->capacity; ++i) {
     if (currently_in_cluster && table->table[i].line_numbers == NULL) {
       currently_in_cluster = false;
+      
       if (current_cluster_size > output->max_cluster_size) {
         output = realloc(output, sizeof(struct HashTablePerformanceHeuristics) + current_cluster_size * sizeof(size_t));
         memset(output->cluster_size_populations + output->max_cluster_size, 0, sizeof(size_t) * (current_cluster_size - output->max_cluster_size));
         output->max_cluster_size = current_cluster_size;
       }
+      
       output->cluster_size_populations[current_cluster_size - 1]++;
       current_cluster_size = 0;
     }
@@ -302,8 +329,6 @@ struct HashTablePerformanceHeuristics* get_substring_hash_table_performance_heur
   if (currently_in_cluster) {
     output->max_cluster_size = current_cluster_size > output->max_cluster_size ? current_cluster_size : output->max_cluster_size;
   }
-
-
 
   return output;
 }
