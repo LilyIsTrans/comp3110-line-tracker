e4e180bbb784ea014ef1c970a1dac05fff853f09
Fawn Barisic <95543319+F-a-w-n@users.noreply.github.com>
2025-11-27 22:35:44 -0500

    Added distance.h and comments

--- src/line-diff/distance.c
+++ src/line-diff/distance.c
@@ -1,10 +1,5 @@
 #include "distance.h"
-#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <errno.h>
 
 #define min(a, b) ((a) < (b) ? (a) : (b))
 
--- src/line-diff/distance.h
+++ src/line-diff/distance.h
@@ -1,6 +1,23 @@
 #pragma once
-#include <stdint.h>
-#include <stdlib.h>
-#include <stdbool.h>
 #include <stdio.h>
-#include <errno.h>
+
+#define min(a, b) ((a) < (b) ? (a) : (b))
+
+/*
+    Calculates the amount of letters added, removed, or modified between two strings
+    takes two char arrays and their lengths, returns the distance between them
+    O(m*n) efficiency where m is length of string 1 and n is length of string 2
+*/
+int levenshteinDistance(const char* str1,
+                            const char* str2, 
+                            const unsigned int len1,
+                            const unsigned int len2);
+
+/*
+    Basic function to test proof of concept for a list of candidates comparing to one line
+    takes in the string, a list of strings to compare to, the length of the list, and a pointer to a char array and int to store the best candidate and its score
+*/
+void closestLine(char* str1, char* strs[], int strscount, char** best, int* bestDiff);
+
+// using main in this program to test closestLine
+int main(int argc, const char **argv);
