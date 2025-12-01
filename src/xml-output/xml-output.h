125df165b7a64c644cc9835818f9d9227153b8d5
Emily <mills13@uwindsor.ca>
2025-12-01 18:46:35 -0500

    Change some function delarations in xml-output.h

--- src/xml-output/xml-output.h
+++ src/xml-output/xml-output.h
@@ -2,21 +2,27 @@
 #include <stdio.h>
 
 /**
- *  starts a new XML version block for the given file.
- *
- * filename				name of the file being tracked.
- * id					id for this version block.
- */
-void start_version_block(const char* filename, int id);
+  *  starts a new XML version block for the given file.
+  *
+  *	 xml_file:	file being tracked.
+  *  filename:	name of the file being tracked.
+  *  id:		id for this version block.
+  */
+void start_version_block(FILE *xml_file, const char* filename, int id);
 
 /**
- *  outputs a (single) line change in XML formatting.
- *
- *  old_line			line number in the old version.
- *  new_line			line number in the new version.
- */
-void write_location(int old_line, int new_line);
+  *  outputs a line change in XML formatting.
+  *
+  *	 xml_file:	file to track
+  *  old_line:	line number in the old version.
+  *  new_line:	line number in the new version.
+  */
+void write_location(FILE *xml_file, int old_line, int new_line);
 
 
-//  ends the current XML version block.
-void end_version_block(void);
+/**
+  *  ends the current XML version block.
+  * 
+  *  xml_file:	file to track
+  */
+void end_version_block(FILE *xml_file);
